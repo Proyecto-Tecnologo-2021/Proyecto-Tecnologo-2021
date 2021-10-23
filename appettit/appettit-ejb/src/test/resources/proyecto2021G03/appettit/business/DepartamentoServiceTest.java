33c3587c711b8b0f76f798f95bdcd80631b423c6
Micaa17 <mglg1704@gmail.com>
2021-10-23 17:42:24 -0300

    Inicio testing

---
+++ appettit/appettit-ejb/src/test/resources/proyecto2021G03/appettit/business/DepartamentoServiceTest.java
@@ -0,0 +1,69 @@
+package proyecto2021G03.appettit.business;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DepartamentoServiceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testCrear() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEliminar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarPorId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscarPorNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrearCiudad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCrearLocalidad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCiudadPorId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocalidadPorId() {
+		fail("Not yet implemented");
+	}
+
+}
