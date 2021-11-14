10dd780be33339de415d704e95f462361fa85df5
mpulidouy <mpulidouy@hotmail.com>
2021-11-14 09:03:56 -0300

    Se corrige MenuRDTO para que incluya las promociones en el listado de menús

--- appettit/appettit-ejb/src/main/java/proyecto2021G03/appettit/converter/PedidoRConverter.java
+++ appettit/appettit-ejb/src/main/java/proyecto2021G03/appettit/converter/PedidoRConverter.java
@@ -1,13 +1,22 @@
 package proyecto2021G03.appettit.converter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 
+import lombok.Builder;
 import proyecto2021G03.appettit.dao.IUsuarioDAO;
+import proyecto2021G03.appettit.dto.ExtraMenuRDTO;
+import proyecto2021G03.appettit.dto.ImagenDTO;
+import proyecto2021G03.appettit.dto.MenuRDTO;
 import proyecto2021G03.appettit.dto.PedidoRDTO;
+import proyecto2021G03.appettit.dto.ProductoRDTO;
+import proyecto2021G03.appettit.dto.PromocionRDTO;
 import proyecto2021G03.appettit.entity.Pedido;
+import proyecto2021G03.appettit.entity.Promocion;
 
 @Singleton
 public class PedidoRConverter extends AbstractConverter<Pedido, PedidoRDTO>{
@@ -45,11 +54,12 @@ public class PedidoRConverter extends AbstractConverter<Pedido, PedidoRDTO>{
     @Override
     public PedidoRDTO fromEntity(Pedido pedido) {
         if(pedido== null) return null;
+        
         PedidoRDTO pedidofinal = PedidoRDTO.builder()
         		.id(pedido.getId())
                 .idcli(pedido.getId_cliente())
                 .iddir(pedido.getId_entrega())
-                .menus(menuRConverter.fromEntityList(pedido.getMenus()))
+                .menus(this.getMenuPromo(pedido))
                 .pago(pedido.getPago())
                 .tipo(pedido.getTipo())
                 .total(pedido.getTotal())
@@ -91,28 +101,34 @@ public class PedidoRConverter extends AbstractConverter<Pedido, PedidoRDTO>{
         		.build();
         
     }
+    
+    private List<MenuRDTO> getMenuPromo(Pedido pedido){
+		
+    	List<MenuRDTO> menus= menuRConverter.fromEntityList(pedido.getMenus());
+    	for(Promocion prom: pedido.getPromociones()) {
+      		
+    		menus.add(new MenuRDTO(
+    				prom.getId(), 
+    				prom.getId_restaurante(),
+    				prom.getRestaurante().getNombre(),
+    				prom.getDescuento(), 
+    				prom.getNombre(), 
+    				prom.getDescripcion(),
+    				prom.getPrecio(), 
+    				prom.getPrecio(), 
+    				null, 
+    				null, 
+    				prom.getId_imagen(), 
+    				null,
+    				"PROM",
+    				-1));
+    		
+        	
+        }
+        return menus;
+    	
+    }
 }
 
-/*
- *  private Long id;
-    private EstadoPedido estado;
-    private TipoPago tipo;
-    private Boolean pago;
-    private Integer tiempoEstimado;
-    private String motivo;
-    private LocalDateTime fecha;
-    private Double total;
-    private RestauranteDTO restaurante;
-    private Cliente cliente;
-    private List<MenuDTO> menus;
-    private List<PromocionDTO> promociones;
-    private List<ExtraMenuDTO> extraMenu;
-    private DireccionDTO entrega;
-    private ReclamoDTO reclamo;
-    private Long id_restaurante;
-	private Long id_cliente;
-	private Long id_entrega;
-	private Long id_reclamo;
 
- */
 
